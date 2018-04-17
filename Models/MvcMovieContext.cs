791bdfa948573fb84a1be37df0e30cf0344561e3
root <root@grzegorz-php72.com>
2018-04-17 17:33:32 +0200

    first commit

---
+++ Models/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# aspnet_core_adding_controller
+
+follow images m7.png, m8.png , m9.png m10.png
